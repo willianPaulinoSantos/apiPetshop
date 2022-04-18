fc54c258d3c8b0153bed21738808d51c54749926
WIllian Paulino <willianpaulinox@gmail.com>
2022-04-18 11:44:36 -0300

    Inserted delete and get products routes and serialized products. Also, adapted the API to work with both JSON and XML files.

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -5,7 +5,38 @@ module.exports = {
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
+            },
+            raw: true
+        })
+    },
+
+    inserir (dados){
+        return Modelo.create(dados)
+    },
+
+    remover (idProduto, idFornecedor){
+        return Modelo.destroy({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
             }
         })
+    },
+
+    async pegarPorId(idProduto, idFornecedor){
+        const encontrado = await Modelo.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
+            },
+            raw: true
+        })
+
+        if(!encontrado){
+            throw new Error('Produto não foi encontrado')
+        }
+
+        return encontrado
     }
+
 }
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,69 @@
 const roteador = require('express').Router({ mergeParams: true })
 const Tabela = require('./TabelaProduto')
+const Produto = require('./Produto')
+const Serializador = require('../../../Serializador').SerializadorProduto
 
 roteador.get('/', async (requisicao, resposta) => {
-    const produtos = await Tabela.listar(requisicao.params.idFornecedor)
-    resposta.send(
-        JSON.stringify(produtos)
+    const produtos = await Tabela.listar(requisicao.fornecedor.id)
+    const serializador = new Serializador(
+        resposta.getHeader('Content-type')
     )
+    resposta.send(
+        serializador.serializar(produtos)
+    )
+})
+
+roteador.post('/', async (requisicao, resposta, proximo) => {
+   try {
+    const idFornecedor = requisicao.fornecedor.id
+    const corpo = requisicao.body
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor })
+    const produto = new Produto(dados)
+    await produto.criar()
+    const serializador = new Serializador(
+        resposta.getHeader('Content-type')
+    )
+    resposta.status(201)
+    resposta.send(
+        serializador.serializar(produto)
+    )
+   } catch (erro) {
+       proximo(erro)
+   }
+})
+
+roteador.delete('/:id', async (requisicao, resposta) => {
+    const dados = {
+        id: requisicao.params.id,
+        fornecedor: requisicao.fornecedor.id
+    }
+
+    const produto = new Produto(dados)
+    await produto.apagar()
+    resposta.status(204)
+    resposta.end()
+})
+
+roteador.get('/:id', async (requisicao, resposta, proximo) => {
+   try {
+        const dados = {
+            id: requisicao.params.id,
+            fornecedor: requisicao.fornecedor.id
+        }
+
+        const produto = new Produto(dados)
+        await produto.carregar()
+        const serializador = new Serializador(
+            resposta.getHeader('Content-type'),
+            ['preco', 'estoque', 'fornecedor', 'dataCriacao', 'dataAtualizacao', 'versao']
+        )
+        resposta.send(
+            serializador.serializar(produto)
+        )
+   } catch (erro){
+       proximo(erro)
+   }
+
 })
 
 module.exports = roteador
